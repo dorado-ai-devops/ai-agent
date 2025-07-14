0fbf6aecd14c9f7943b66c5dfbb9b7a9444a1e55
root <root@deletepc.localdomain>
2025-07-15 01:46:57 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- server.py
+++ server.py
@@ -32,4 +32,4 @@ async def ask(request: Request):
 
 # Opcional: correr directamente con python main_api.py
 if __name__ == "__main__":
-    uvicorn.run("main_api:app", host="0.0.0.0", port=6001, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=6001, reload=True)
