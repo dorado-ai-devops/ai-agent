aad3aac800d780cdb151a0d924c44dd3856d8d13
root <root@deletepc.localdomain>
2025-07-15 17:14:44 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,13 +2,19 @@
 
 from .ai_gateway_tools import generate_pipeline_tool, analyze_log_tool, lint_chart_tool
 from .list_repos_tool import list_github_repos
-from .download_chart_tool import fetch_helm_chart 
+from .download_chart_tool import fetch_helm_chart
+from .get_github_readme import  get_github_r
+from .analyze_helm import analyze_helm_chart
+
 tools = [
     generate_pipeline_tool,
     analyze_log_tool,
     lint_chart_tool,
     list_github_repos,
-    fetch_helm_chart
+    fetch_helm_chart,
+    get_github_r,
+    analyze_helm_chart
+
 
 ]
 
